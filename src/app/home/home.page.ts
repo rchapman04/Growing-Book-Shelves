45d5717554a595455a63c3cce1635dd60f3a8ba0
Reid Chapman <rmchapman2004@gmail.com>
2025-11-19 14:11:09 -0600

    Updated the program so now each book will create an onclick event when the page is loaded, currently just console logs the interaction

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   styleUrls: ['home.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
+
+function bookDescription(){
+  const newIonCard = document.createElement("ion-card")
+
+  
+}
+
 export class HomePage {
   constructor() {}
+
+  ngAfterViewInit(){
+    const books = document.querySelectorAll(".book")
+
+    books.forEach(book => {
+      book.addEventListener("click", () => {
+        console.log("You have clicked a book!")
+      })
+    })
+  }
+
 }
